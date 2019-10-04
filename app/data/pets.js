53e468896316be12dfbda6f2ef6980e7cb208247
George Wyscaver <gwyscaver3@gmail.com>
2019-10-03 18:11:10 -0700

    finished writing code

---
+++ app/data/pets.js
@@ -0,0 +1,24 @@
+var pets = [
+    {
+        name:"dog",
+        scores:[5,4,3,2,1]
+    },
+    {
+        name:"cat",
+        scores:[1,2,3,4,5]
+    },
+    {
+        name:"bunny",
+        scores:[3,2,1,4,5]
+    },
+    {
+        name:"fish aquarium",
+        scores:[3,4,5,2,1]
+    },
+    {
+        name:"bird",
+        scores:[4,5,1,2,3]
+    },
+
+]
+module.exports=pets
